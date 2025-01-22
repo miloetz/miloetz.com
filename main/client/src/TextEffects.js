e0ec27e1aab8409ec5ac956697d3e0310c5f6d9a
miloetz <111991053+miloetz@users.noreply.github.com>
2025-01-21 18:09:49 -0600

    fix something

--- main/client/src/TextEffects.js
+++ main/client/src/TextEffects.js
@@ -13,16 +13,4 @@ const WiggleText = ({ text }) => {
     );
 };
 
-const WiggleTextNav = ({ text }) => {
-    return (
-        <span className="wiggle-text">
-            {text.split('').map((char, index) => (
-                <span key={index} className="wiggle-letter" style={{ '--wiggle-index': index }}>
-                    {char}
-                </span>
-            ))}
-        </span>
-    );
-};
-
 export default WiggleText;
