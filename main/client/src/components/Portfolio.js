aef758700c9305ed588cc22623cbf4cabff17442
miloetz <111991053+miloetz@users.noreply.github.com>
2025-08-19 17:22:18 -0500

    Quick Add

--- main/client/src/components/Portfolio.js
+++ main/client/src/components/Portfolio.js
@@ -8,8 +8,33 @@ function Portfolio() {
                 <h1>Portfolio</h1>
                 <img src="/assets/pictures/Milo_Etz_ShapeColorTexture.png" alt="Milo Etz Shape Color Texture" />
                 <p>untitled</p>
+                <img src="https://i.imgur.com/sEeo0As.png" alt="Untitled Art" />
+                <p>untitled</p>
                 <img src="https://i.imgur.com/uADVDBT.gif" alt="Untitled Art" />
                 <p>untitled</p>
+                <img src="https://i.imgur.com/iVH6yth.jpeg" alt="Untitled Art" />
+                <p>untitled</p>
+                <img src="https://i.imgur.com/ftYWqU7.png" alt="Untitled Art" />
+                <p>untitled</p>
+                <img src="https://i.imgur.com/zCcVt0P.png" alt="Untitled Art" />
+                <p>untitled</p>
+                <img src="https://i.imgur.com/dpV56ta.png" alt="Untitled Art" />
+                <p>untitled</p>
+                <img src="https://i.imgur.com/LEKBF12.png" alt="Untitled Art" />
+                <p>untitled</p>
+                <img src="https://i.imgur.com/vF3zoSQ.jpeg" alt="Untitled Art" />
+                <p>untitled</p>
+                <img src="https://i.imgur.com/LXYQGIN.jpeg" alt="Untitled Art" />
+                <p>untitled</p>
+                <img src="https://i.imgur.com/j2ZcUGs.jpeg" alt="Untitled Art" />
+                <p>untitled</p>
+                <img src="https://i.imgur.com/P2ZjuZZ.jpeg" alt="Untitled Art" />
+                <p>untitled</p>
+                <img src="https://i.imgur.com/lpYYsst.jpeg" alt="Untitled Art" />
+                <p>untitled</p>
+                <img src="https://i.imgur.com/NemCy9q.jpeg" alt="Untitled Art" />
+                <p>untitled</p>
+                <img src="https://i.imgur.com/ePMe9xN.png" alt="Untitled Art" />
             </header>
         </div>
     );
